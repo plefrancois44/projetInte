d11cb015f1f30373f4e264467703b46d536ea49b
melanieservais <melanie.servais@imerir.com>
2017-06-26 14:31:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -357,9 +357,9 @@ def get_metrology():
 			   {"dfn" : 1,
 			   "weather" : "sunny"}
 		   ]}
-	test = arduino[0][weather[0][weather]]
-	retour = make_response(json.dumps(arduino),200)
-	return test
+	test = arduino[0][weather]
+	retour = make_response(json.dumps(test),200)
+	return retour
 
 #----------------------------------- LANCE L'APP -----------------------------------#
 if __name__ == "__main__":
