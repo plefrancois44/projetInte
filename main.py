742edecf8abd49bdeba0e7257cfd4de5d9a40ad9
leroyjenkins66 <pierre.brosa@imerir.com>
2017-06-27 13:09:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -143,7 +143,7 @@ def action_player(player):
 		i = 0
 		for recette in range(0,len(recetteJoueur)):
 			prepare = data["prepare"][i]
-			i++
+			i=i+1
 			nb = int(prepare["quantite"])
 			ingredient = {}
 			cout=[]
