fdac606e0517d51edf7afc7912a5baf830c109f9
melanieservais <melanie.servais@imerir.com>
2017-06-27 10:03:04 +0200

    Update base.sql

--- base.sql
+++ base.sql
@@ -17,7 +17,7 @@ CREATE TABLE public.Ingredient(
 );
 
 CREATE TABLE public.Recette(
-        rec_id int Auto_increment NOT NULL,
+        rec_id SERIAL NOT NULL,
 	rec_nom        Varchar (50) NOT NULL ,
 	rec_jour_achat Int,
         rec_cout_achat Float ,
