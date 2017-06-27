a2bc15e4cea08ec07083c09cb15431da562d8259
plefrancois44 <pierre.lefrancois@imerir.com>
2017-06-27 11:37:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def action_player(player):
 			cout=[]
 			coutProd = 0.0
 			recettes[recette]=(db.select("SELECT * FROM composer WHERE rec_nom=@(recette) AND jou_nom=@(nom)", 
-				{'recette' : recetteJoueur[recette]["rec_nom"], 'nom' : data['user']}))
+				{'recette' : recetteJoueur[recette]["rec_nom"], 'nom' : player}))
 			ingredientRecette = recettes[recette]
 			for ingredient in range(0,len(ingredientRecette)):
 				cout += (db.select("SELECT ing_prix_unitaire FROM Ingredient WHERE ing_nom=@(ing)", {'ing' : ingredientRecette[ingredient]["ing_nom"]}))
