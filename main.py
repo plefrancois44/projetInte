f2274423c226ed2488e7caf51517056fe3a4072d
melanieservais <melanie.servais@imerir.com>
2017-06-27 09:19:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -353,8 +353,8 @@ def post_players():
 			ingredientRecette = recettes[recette]
 			for ingredient in range(0,len(ingredientRecette)):
 				cout = (db.select("SELECT ing_prix_unitaire FROM Ingredient WHERE ing_nom=@(ing)", {'ing' : ingredientRecette[ingredient]["ing_nom"]}))
-				print(cout)
-				coutProd = coutProd + cout
+				print(cout[0]['ing_prix_unitaire'])
+				coutProd = coutProd + cout[0]['ing_prix_unitaire']
 
 				ingredients[ingredient]=(db.select("SELECT ing_alcool FROM Ingredient WHERE ing_nom=@(ing)", {'ing' : ingredientRecette[ingredient]["ing_nom"]}))
 				if ingredients[ingredient]["ing_alcool"] == True & alcool == False :
