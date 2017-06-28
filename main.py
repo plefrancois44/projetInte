5aacb21cdd757c3ff60283ff494c140815acfb20
plefrancois44 <pierre.lefrancois@imerir.com>
2017-06-28 19:33:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def sales():
 	element = {}
 	
 	meteoJour = db.select("SELECT met_jour FROM meteo WHERE met_apres_midi IS NOT NULL ORDER BY met_jour DESC LIMIT 1")
-	jour = meteo[0]['met_jour']
+	jour = meteoJour[0]['met_jour']
 	
 	verif = db.select("SELECT * FROM vendre WHERE rec_nom=@(recette) AND ven_jour=(@(jour) AND jou_nom=@(nom);",
 		{ 
