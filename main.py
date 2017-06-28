e66225d1e7b2601f70a2644e2a4f4eb4d88fecc2
melanieservais <melanie.servais@imerir.com>
2017-06-28 14:14:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -644,9 +644,8 @@ def get_map_player(playerName):
 		drinksInfos += drinkInfo
 
 	riche=[]
-	maximum=[]
-	prem=[]
 	maximum = (db.select("SELECT MAX(jou_budget) AS maximum FROM joueur")
+	print(maximum)
 	prem = (db.select("SELECT * FROM joueur WHERE jou_budget=@(maximum)",{'maximum' : maximum[0]["maximum"]}))
 	joueurPrem = 0
 	if len(prem)>1:
